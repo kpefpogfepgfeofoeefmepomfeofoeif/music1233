0aa438deae7f1654ab5f63690500e07ab4da30d0
kpefpogfepgfeofoeefmepomfeofoeif <43859071+kpefpogfepgfeofoeefmepomfeofoeif@users.noreply.github.com>
2018-11-22 08:38:21 +0300

    Update bot-6.js

--- bot-6.js
+++ bot-6.js
@@ -20,9 +20,9 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "ti";
 /////////////////////////
-////////////////////////
+///////////////////////
 
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
@@ -313,7 +313,7 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-    if (message.content === 'help') {
+    if (message.content === 'tihelp') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
         .setDescription('**برفكس البوت (!)**')
@@ -331,7 +331,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'general_commands') {
+    if (message.content === 'tigeneral_commands') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر عامة...**')
         .addField('avatar', "افاتار الشخص المطلوب")
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Type `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
